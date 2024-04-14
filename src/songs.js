bf3cc8e84c15552905223ed3a9b1d0565a47ac10
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2024-04-14 23:10:56 +0200

    finished exercise 1 and 2

--- src/songs.js
+++ src/songs.js
@@ -1,44 +1,49 @@
 //Exercise 1: Get the array of all Artists.
-function getAllArtists(array){
-    let result = '???';
-    //console.log("Exercise 1 ->", result);
-    return result;
-};
+function getAllArtists(array) {
+  let result = array.map((songs) => songs.artist);
+  //console.log("Exercise 1 ->", result);
+  return result;
+}
 
 //Exercise 2: Get the songs of a certain artist
-function getSongsFromArtist(array, artist){
-    //Write your code here
-};
+function getSongsFromArtist(array, artist) {
+  return array.filter((songs) => songs.artist === artist);
+}
 
 //Exercise 3: Alphabetic order by title
-function orderAlphabetically(){
-    //Write your code here
-};
+function orderAlphabetically() {
+  return array.sort((a, b) => a.title.localeCompare(b.title));
+}
 
 //Exercise 4: Order by year, ascending
-function orderByYear(){
-    //Write your code here
-};
+function orderByYear() {
+  //Write your code here
+}
 
 //Exercise 5: Filter songs by genre
 function songsByGenre() {
-    //Write your code here
-};
+  //Write your code here
+}
 
 //Exercise 6: Modify the duration of songs to seconds
 function minutsToSeconds() {
-    //Write your code here
-};
+  //Write your code here
+}
 
 //Exercise 7: Get the longest song
-function getLongestSong(){
-    //Write your code here
-};
+function getLongestSong() {
+  //Write your code here
+}
 
 //Exercise 8: Get the shortest song
 //Write the getShortestSong() function
 
-
-
-
-export { getAllArtists, getSongsFromArtist, orderAlphabetically, orderByYear, songsByGenre, minutsToSeconds, getLongestSong };
+export {
+  getAllArtists,
+  getSongsFromArtist,
+  orderAlphabetically,
+  orderByYear,
+  songsByGenre,
+  minutsToSeconds,
+  getLongestSong,
+};
